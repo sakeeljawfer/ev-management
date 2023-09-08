175b092ff1991ef931da7a6bf6f44b078aba5f1e
Sakeel Jawfer <37551196+sakeeljawfer@users.noreply.github.com>
2023-09-08 15:18:40 +0530

    slugs-alerts

--- nextjs-proapp/pages/events/index.js
+++ nextjs-proapp/pages/events/index.js
@@ -2,12 +2,21 @@ import { getAllEvents } from '../../dummy-data'
 import EventList from '../../components/events/event-list';
 import { Fragment } from 'react';
 import EventsSearch from '../../components/events/events-search'
+import { useRouter } from 'next/router';
 export default function Events() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
+
   return (
     <>
       <Fragment>
-        <EventsSearch />
+        <EventsSearch onSearch={findEventsHandler} />
         <EventList items={events} />
       </Fragment>
 
